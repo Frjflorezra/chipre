49dab59683d40ba3c95291e769d1541507f320bc
Frjflorezra <franciscojflorezr@gmail.com>
2025-11-15 15:15:06 -0500

    Creation project

---
+++ settings.gradle.kts
@@ -0,0 +1,15 @@
+pluginManagement {
+    repositories {
+        maven { url = uri("https://repo.spring.io/snapshot") }
+        gradlePluginPortal()
+    }
+}
+rootProject.name = "chirp"
+
+include("user")
+include("app")
+
+
+include("chat")
+include("common")
+include("notification")
